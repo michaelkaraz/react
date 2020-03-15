f73dcc6f4a5d271b191f1c6afe14b1db592d7eb8
Michael Karamanolis <michaelkaraz@gmail.com>
2020-03-15 10:53:15 +0200

    Updates

---
+++ precache-manifest.231004cf49afb1b7a4071878de207b7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c17198cda3b96622e1bb06eb9f334a3c",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "de38bd023d095fe54e05",
+    "url": "/react/static/css/main.52fba5e0.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/react/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de38bd023d095fe54e05",
+    "url": "/react/static/js/main.b3809d23.chunk.js"
+  },
+  {
+    "revision": "922a25a4137c7190bdfa",
+    "url": "/react/static/js/runtime-main.cce4a0d6.js"
+  }
+]);
